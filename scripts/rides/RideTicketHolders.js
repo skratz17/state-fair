a88facd18b33f2998a0725a6f9bea86ae39b3c84
Jacob Eckert <jweckert17@gmail.com>
2020-07-27 14:40:27 -0500

    tickets to ride

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,8 +1,9 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
-export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
-    })
-}
+eventHub.addEventListener('rideTicketPurchased', () => {
+    contentTarget.innerHTML += '<div class="person rider"></div>';
+});
 
+export const RideTicketHolders = () => {
+};
