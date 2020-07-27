a8c745f11c5951f326b185c8fb985708af60f0f1
Jacob Eckert <jweckert17@gmail.com>
2020-07-27 14:57:40 -0500

    implemented food button

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,8 +1,9 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener('rideTicketPurchased', () => {
-    contentTarget.innerHTML += '<div class="person rider"></div>';
+eventHub.addEventListener('rideTicketPurchased', event => {
+    const className = event.detail.type === 'rideTicket' ? 'rider' : '';
+    contentTarget.innerHTML += `<div class="person ${className}"></div>`;
 });
 
 export const RideTicketHolders = () => {
